01f014905fe23a9626cf688ad7441cf525974ac4
yuze <jun.yuze@gmail.com>
2019-01-25 20:46:55 +0900

    mod init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -16,7 +16,7 @@
 (setq transient-mark-mode t)
 (setq inhibit-startup-message t)
 (setq make-backup-files nil)
-(setq default-tab-width 4)
+(setq default-tab-width 2)
 (setq frame-title-format (concat  "%b - emacs@" (system-name)))
 (setq diff-switches "-u")
 (menu-bar-mode -1)
@@ -25,33 +25,27 @@
 (global-set-key "\C-x\C-g" 'goto-line)
 
 ;; packages
-;;; web-mode
-(straight-use-package 'web-mode)
-(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.vue\\'" . web-mode))
-(defun web-mode-hook ()
-  (setq web-mode-html-offset   4)
-  (setq web-mode-css-offset    4)
-  (setq web-mode-script-offset 4)
-  (setq web-mode-php-offset    4)
-  (setq indent-tabs-mode nil)
-  (setq tab-width 4))
-(add-hook 'web-mode-hook 'web-mode-hook)
 
-;;; js2-mode
-(straight-use-package 'js2-mode)
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+;; helm-themes
+(straight-use-package 'helm-themes)
+(load-theme 'wombat t t)
+(enable-theme 'wombat)
 
-;;; helm
+;; helm
 (straight-use-package 'helm)
+(helm-mode 1)
 (define-key global-map (kbd "M-y") 'helm-show-kill-ring)
 (define-key global-map (kbd "C-x C-f") 'helm-find-files)
 (define-key global-map (kbd "C-x C-r") 'helm-recentf)
+(define-key helm-map (kbd "C-h") 'helm-ff-delete-char-backward)
+(define-key helm-find-files-map (kbd "TAB") 'helm-execute-persistent-action)
 
-;;; recentf
+;; recentf
 (straight-use-package 'recentf-ext)
-(setq recentf-max-saved-items 100)
+(recentf-mode 1)
+(setq recentf-max-saved-items 200)
+(setq recentf-save-file "~/.emacs.d/recentf")
+(setq recentf-auto-cleanup 'never)
 (setq helm-for-files-preferred-list
       '(helm-source-buffers-list
         helm-source-recentf
@@ -61,29 +55,7 @@
         helm-source-bookmark-set
         helm-source-locate))
 
-;;; migmo
-(straight-use-package 'migemo)
-(straight-use-package 'helm-migemo)
-(setq migemo-command "cmigemo")
-(setq migemo-options '("-q" "--emacs"))
-(setq migemo-dictionary "/usr/local/Cellar/cmigemo/20110227/share/migemo/utf-8/migemo-dict")
-(setq migemo-user-dictionary nil)
-(setq migemo-regex-dictionary nil)
-(setq migemo-coding-system 'utf-8-unix)
-(load-library "migemo")
-(migemo-init)
-
-;;; migemo + swiper
-(straight-use-package 'avy)
-(straight-use-package 'swiper-helm)
-(straight-use-package 'avy-migemo)
-(avy-migemo-mode 1)
-(require 'avy-migemo-e.g.swiper)
-;;; swiper
-(global-set-key "\C-s" 'swiper)
-(defvar swiper-include-line-number-in-search t)
-
-;;; company
+;; company
 (straight-use-package 'company)
 (global-company-mode)
 (setq company-transformers '(company-sort-by-backend-importance))
@@ -103,3 +75,58 @@
 (define-key company-active-map (kbd "C-f") 'company-complete-selection)
 (define-key emacs-lisp-mode-map (kbd "C-M-i") 'company-complete)
 
+;; web-mode
+(straight-use-package 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.html\\'" . web-mode))
+(defun web-mode-hook ()
+  (setq web-mode-html-offset   2)
+  (setq web-mode-css-offset    2)
+  (setq web-mode-script-offset 2)
+  (setq indent-tabs-mode nil)
+  (setq tab-width 2))
+(add-hook 'web-mode-hook 'web-mode-hook)
+
+;; php-mode
+;(straight-use-package 'cl)
+(straight-use-package 'php-mode)
+(straight-use-package 'company-php)
+(add-hook 'php-mode-hook
+          '(lambda ()
+             (company-mode t)
+             (ac-php-core-eldoc-setup)
+             (make-local-variable 'company-backends)
+             (add-to-list 'company-backends 'company-ac-php-backend)))
+
+;; js2-mode
+(straight-use-package 'js2-mode)
+(straight-use-package 'company-tern)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-hook 'js2-mode-hook 'tern-mode) 
+(add-to-list 'company-backends 'company-tern)
+
+;;; flycheck
+(straight-use-package 'flycheck)
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+;; migmo
+(straight-use-package 'migemo)
+(straight-use-package 'helm-migemo)
+(setq migemo-command "cmigemo")
+(setq migemo-options '("-q" "--emacs"))
+(setq migemo-dictionary "/usr/local/Cellar/cmigemo/20110227/share/migemo/utf-8/migemo-dict")
+(setq migemo-user-dictionary nil)
+(setq migemo-regex-dictionary nil)
+(setq migemo-coding-system 'utf-8-unix)
+(load-library "migemo")
+(migemo-init)
+
+;; swiper
+(global-set-key "\C-s" 'swiper)
+(defvar swiper-include-line-number-in-search t)
+
+;; migemo + swiper
+(straight-use-package 'avy)
+(straight-use-package 'swiper-helm)
+(straight-use-package 'avy-migemo)
+(avy-migemo-mode 1)
+(require 'avy-migemo-e.g.swiper)
